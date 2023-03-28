88bafd27de2914240aef028ddf094c5e0e3927b5
ThisisXXZ <xxz.7203@gmail.com>
2023-03-28 20:27:36 +0800

    Chessboard module updated.

--- chessboard.h
+++ chessboard.h
@@ -8,11 +8,17 @@ class chessboard{
 public:
     chessboard();
     void print();
-    int setPiece(int x, int y, int p); // p (party): 0 for black, 1 for white
-                                       // setPiece has return value: 1 for improper instructions
-    int chkSuccess();                  // return -1 if game should proceed; 
-                                       // otherwise return the winning party
 
+    int getPiece(int x, int y);        // -1(X) for black, 0 for unoccupied, 1(O) for white
+    int setPiece(int x, int y, int p); // p (party): -1(X) for black, 1(O) for white
+                                       // setPiece() has return value: 1 for improper instructions
+    int chkSuccess();                  // return 0 if game should proceed; 
+                                       // otherwise return the winning party
+    int chkVerticle(int x, int y);
+    int chkHorizontal(int x, int y);
+    int chkLeftSlant(int x, int y);    
+    int chkRightSlant(int x, int y);   // 4 subfuntions for chkSuccess(); return value has the same logic
+                                       // as chkSuccess()
 };
 
 #endif
