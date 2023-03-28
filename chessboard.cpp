92d4772185e6310754efb77932ae2cef59dd791b
ThisisXXZ <xxz.7203@gmail.com>
2023-03-28 20:26:29 +0800

    Chessboard module updated. New test cases added in main.cpp

--- chessboard.cpp
+++ chessboard.cpp
@@ -25,14 +25,59 @@ void chessboard::print() {
     }
 }
 
+int chessboard::getPiece(int x, int y) {
+    if (b[x][y] == 'X') return -1;
+    if (b[x][y] == 'O') return 0;
+    return 0;
+}
+
 int chessboard::setPiece(int x, int y, int p) {
     if (x > 15 || x < 1 || y > 15 || y < 1) 
         return 1;
     if (b[x][y] != ' ')
         return 1;
-    b[x][y] = (p == 0) ? 'X' : 'O'; 
+    b[x][y] = (p == -1) ? 'X' : 'O'; 
+    return 0;
+}
+
+int chessboard::chkVerticle(int x, int y) {
+    if (b[x][y] == ' ' || y > 11)
+        return 0;
+    for (int _y = y; _y < y + 5; ++_y) {
+        if (b[x][_y] != b[x][y])    return 0;
+    } 
+    return (b[x][y] == 'X') ? -1 : 1;
+}
+
+int chessboard::chkHorizontal(int x, int y) {
+    if (b[x][y] == ' ' || x > 11)
+        return 0;
+    for (int _x = x; _x < x + 5; ++_x) {
+        if (b[_x][y] != b[x][y])    return 0;
+    }
+    return (b[x][y] == 'X') ? -1 : 1;
+}
+
+int chessboard::chkRightSlant(int x, int y) {
+    if (b[x][y] == ' ' || x > 11 || y > 11)
+        return 0;
+}
+
+int chessboard::chkLeftSlant(int x, int y) {
+    if (b[x][y] == ' ' || x > 11 || y < 5)
+        return 0;
 }
 
 int chessboard::chkSuccess() {
-    
+    for (int x = 1; x <= 15; ++x)
+        for (int y = 1; y <= 15; ++y) {
+            int res = chkVerticle(x, y);
+            if (res)    return res;
+            res = chkHorizontal(x, y);
+            if (res)    return res;
+            res = chkRightSlant(x, y);
+            if (res)    return res;
+            res = chkLeftSlant(x, y);
+        }
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -10,5 +10,13 @@ int main() {
     chessboard a;
     a.print();
 
+    for (int i = 1; i <= 5; ++i)
+        a.setPiece(i, 3, -1);
+    
+    cout << endl;
+    a.print();
+    cout << a.getPiece(4, 3) << endl;
+    cout << a.chkSuccess() << endl;
+
     return 0;
 }
