da18decd2eb4d497ef01806dcc316f27e73c7781
DorisssW <124768334+DorisssW@users.noreply.github.com>
2023-04-12 13:34:04 +0800

    Update main.cpp
    Added an introduction and a time delay function(to print out the text with a time delay) which makes it easier for readers to read.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+
+void delay() 
+{
+    //延时cout
+    clock_t start_time;//, cur_time;
+    start_time = clock();//clock()
+    for (; (clock() - start_time) < 2 * CLOCKS_PER_SEC;);//延迟2秒
+}  
+
+
 void printTitle() {
     cout << "\
                                   '##::::'##:'##:::'##:'##::::'##:\n\
@@ -30,8 +40,19 @@ void printTitle() {
     '##::: ##:: ##:: ##:.:: ##: ##:::: ##: ##::::::: ##.... ##:::: ##:::: ##:::: ##: ##::. ##::\n\
     . ######::'####: ##:::: ##:. #######:: ########: ##:::: ##:::: ##::::. #######:: ##:::. ##:\n\
     :......:::....::..:::::..:::.......:::........::..:::::..:::::..::::::.......:::..:::::..::\n";
-
+    
+    delay();
     cout << "\nWELCOME!\n\n";
+    delay();
+    cout << "Welcome to the text-based game of university life simulator! In this game, you will play as a freshman starting your journey at the prestigious Hong Kong University (HKU).";
+    delay();
+    cout << "You are excited to begin this new chapter of your life and to explore all the opportunities that come with university education.";
+    delay();
+    cout << "As a player, you will navigate through different events and situations that simulate various aspects of university life, such as academics, social activities, and personal relationships.";
+    delay();
+    cout << "Every decision you make will have consequences, and your ultimate goal is to achieve success while maintaining a balance between your academic and personal priorities.";
+    delay();
+    cout << "Are you ready to embark on this exciting and unpredictable journey as a freshman at HKU? Let's begin and see where life takes you on campus!";
 }
 
 string input() {
